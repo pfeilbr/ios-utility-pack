7682afcaa4e81d6d213fdf43a0be95834913a60f
Brian Pfeil <brian.pfeil@gmail.com>
2011-12-02 23:00:52 -0500

    Initial Commit

---
+++ ios-utility-pack/ios_utility_pack.h
@@ -0,0 +1,13 @@
+//
+//  ios_utility_pack.h
+//  ios-utility-pack
+//
+//  Created by Brian Pfeil on 12/2/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ios_utility_pack : NSObject
+
+@end
---
+++ ios-utility-pack/ios_utility_pack.m
@@ -0,0 +1,23 @@
+//
+//  ios_utility_pack.m
+//  ios-utility-pack
+//
+//  Created by Brian Pfeil on 12/2/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "ios_utility_pack.h"
+
+@implementation ios_utility_pack
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
